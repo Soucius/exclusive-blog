c85b2e2939631b88f92219662755c340696fbd1c
Soucius <soucius.ggwp@gmail.com>
2025-07-28 09:41:03 +0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
+      <div className="flex items-center gap-4 text-2xl font-bold">
+        <img src="/logo.png" className="w-8 h-8" alt="" />
+
+        <span>exclog</span>
+      </div>
+
+      <div className="md:hidden">
+        <div
+          className="cursor-pointer text-2xl"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          {open ? "✖" : "➕"}
+        </div>
+
+        <div
+          className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out bg-[#e6e6ff] ${
+            open ? "-right-0" : "-right-[100%]"
+          }`}
+        >
+          <a href="/">Home</a>
+          <a href="/">Trending</a>
+          <a href="/">Most Popular</a>
+          <a href="/">About</a>
+          <a href="/">
+            <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+              Login 👋
+            </button>
+          </a>
+        </div>
+      </div>
+
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <a href="/">Home</a>
+        <a href="/">Trending</a>
+        <a href="/">Most Popular</a>
+        <a href="/">About</a>
+        <a href="/">
+          <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+            Login 👋
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
