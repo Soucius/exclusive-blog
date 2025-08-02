95c1b4fcdc16a2669dc82d024db44bb42b02e881
Soucius <soucius.ggwp@gmail.com>
2025-08-02 12:08:49 +0300

    react router dom

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,22 @@
 import { useState } from "react";
 import Image from "./Image";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   const [open, setOpen] = useState(false);
 
   return (
     <div className="w-full h-16 md:h-20 flex items-center justify-between">
-      <div className="flex items-center gap-4 text-2xl font-bold">
+      <Link to="/" className="flex items-center gap-4 text-2xl font-bold">
         <Image
-          src="https://ik.imagekit.io/g7gszpv1d/logo.png?updatedAt=1753696265702"
+          src="https://ik.imagekit.io/g7gszpv1d/logo.png"
           alt="Exclusive Logo"
           w={32}
           h={32}
         />
 
         <span>exclog</span>
-      </div>
+      </Link>
 
       <div className="md:hidden">
         <div
@@ -30,28 +31,28 @@ function Navbar() {
             open ? "-right-0" : "-right-[100%]"
           }`}
         >
-          <a href="/">Home</a>
-          <a href="/">Trending</a>
-          <a href="/">Most Popular</a>
-          <a href="/">About</a>
-          <a href="/">
+          <Link to="/">Home</Link>
+          <Link to="/posts">Posts</Link>
+          <Link to="">Most Popular</Link>
+          <Link to="">About</Link>
+          <Link to="">
             <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
               Login 👋
             </button>
-          </a>
+          </Link>
         </div>
       </div>
 
       <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
-        <a href="/">Home</a>
-        <a href="/">Trending</a>
-        <a href="/">Most Popular</a>
-        <a href="/">About</a>
-        <a href="/">
+        <Link to="/">Home</Link>
+        <Link to="/posts">Posts</Link>
+        <Link to="">Most Popular</Link>
+        <Link to="">About</Link>
+        <Link to="">
           <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
             Login 👋
           </button>
-        </a>
+        </Link>
       </div>
     </div>
   );
